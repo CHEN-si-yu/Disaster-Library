5ec4cdd151d54d6d5376db5a5fe00df207bd67de
ste <1456964303@qq.com>
2021-10-27 20:43:43 +0800

    服务器版本

--- data/common.inc.php
+++ data/common.inc.php
@@ -1,9 +1,9 @@
 <?php
 //数据库连接信息
 $cfg_dbhost = 'localhost';
-$cfg_dbname = 'dedegbkweb217';
-$cfg_dbuser = 'root';
-$cfg_dbpwd = '123456';
+$cfg_dbname = 'rgzhtsg';
+$cfg_dbuser = 'rgzhtsg';
+$cfg_dbpwd = 'u8s7f8b8';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'gbk';
 
